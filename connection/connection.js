7025ea4db4e4753772f93ce2392e754f5d6708d9
RyanSzczepanski <Slyry11@gmail.com>
2021-11-14 14:00:19 -0500

    Created Express Connection file

---
+++ connection/connection.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require(`express-session`);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Sets Handlebars as the default template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Sets up the sessions with the 'secret', 'resave', 'saveUninitialized' options
+app.use(
+  session({
+    secret: 'My little secret!',
+    resave: false,
+    saveUninitialized: false
+  })
+);
