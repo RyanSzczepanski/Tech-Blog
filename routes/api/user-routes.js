c8d920f9f664ebd050da1188ad3278d1191e3e6e
RyanSzczepanski <Slyry11@gmail.com>
2021-11-14 16:00:02 -0500

    added creating user and logging out

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,8 +1,28 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
+router.post('/', (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+  
+        res.json(dbUserData);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 router.post("/login", (req, res) => {
-  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
       email: req.body.email,
@@ -28,6 +48,17 @@ router.post("/login", (req, res) => {
       res.json({ user: dbUserData, message: "You are now logged in!" });
     });
   });
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
