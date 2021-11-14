2df1cdc4bc239ec93982089d9d1e758f87372c47
RyanSzczepanski <Slyry11@gmail.com>
2021-11-14 15:46:29 -0500

    updated front end routes

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,19 +1,20 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-    res.render('homepage', {
-        // posts,
-        // loggedIn: req.session.loggedIn
-      });
+  console.log(req.session);
+  res.render("homepage", {
+    // posts,
+    loggedIn: req.session.loggedIn
+  });
 });
 
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render('login');
-  });
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
 
 module.exports = router;
