220e058e119bbfd693e36e17d02dc9270b28e47d
RyanSzczepanski <Slyry11@gmail.com>
2021-11-14 16:57:34 -0500

    updated home routes

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,11 +1,38 @@
 const router = require("express").Router();
+const sequelize = require('../connection/connection');
+const { Post, User, Comment, Vote} = require('../models');
 
 router.get("/", (req, res) => {
-  console.log(req.session);
-  res.render("homepage", {
-    // posts,
-    loggedIn: req.session.loggedIn
-  });
+  Post.findAll({
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+  .then(postData => {
+    const posts = postData.map(post => post.get({ plain: true }));
+    res.render("homepage", {
+      posts,
+      loggedIn: req.session.loggedIn
+    });
+  })
 });
 
 router.get("/login", (req, res) => {
